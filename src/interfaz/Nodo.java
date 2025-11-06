0665a47bbb7bf2be63500144dc1e9601a2cf29af
danipacheco205 <danipacheco@correo.unimet.edu.ve>
2025-11-06 19:32:01 -0400

    Add files via upload

--- src/interfaz/Nodo.java
+++ src/interfaz/Nodo.java
@@ -22,7 +22,7 @@ public class Nodo /**Usuarios*/
     public void addusers (String newuser)
     {
         /**
-         * Agrega un nuevo usuario al arreglo de Strings users
+         * Agrega un nuevo usuario (newuser) al arreglo de Strings users
          * Crea un arreglo temporal llamado adduser con un tamaño de 1 posición más y realiza una copia del arreglo users
          * Agrega el nuevo usuario a la última posición siendo el tamaño menos un índice
          * Establece que el arreglo users es el arreglo adduser
@@ -39,7 +39,7 @@ public class Nodo /**Usuarios*/
     public boolean userexists(String user)
     {
         /**
-         * Verifica si ya existe el usuario que se desea agregar
+         * Verifica si ya existe el usuario que se desea agregar (user)
          * Crea un arreglo temporal llamado allusers con el mismo tamaño del arreglo de la clase users
          * Itera sobre los valores de allusers y lee la información que contiene cada posición del arreglo temporal
          * Retorna true si encuentra un usuario con el mismo nombre que el que se quiere agregar
@@ -60,8 +60,13 @@ public class Nodo /**Usuarios*/
     public void deleteuser(String deluser)
     {
         /**
-         * Elimina el usuario solicitado del arreglo
-         * 
+         * Elimina el usuario solicitado del arreglo (deluser)
+         * Crea un nuevo arreglo temporal llamado tmpuser donde el tamaño de este arreglo es el tamaño del arreglo de la clase usuarios menos un índice 
+         * Crea una variable numerica llamada indx que se le asigna el valor de 0
+         * Itera sobre todos los valores del arreglo users 
+         * Si el valor del arreglo users es distinto de deluser, rellena el arreglo en ese índice con la misma información, además de que indx aumenta en uno
+         * Si coincide no lo agrega al arreglo y continua con la iteración
+         * Establece que el arreglo de la clase users es igual a tmpuser
          */
         String[] tmpuser = new String[this.users.length - 1];
         int indx = 0;
